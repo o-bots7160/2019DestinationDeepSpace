ca6cb339d6f2d1ef9b9414a9a7a9eb7af528f291
LudingtonRobotics <34400009+LudingtonRobotics@users.noreply.github.com>
2019-02-14 16:57:31 -0500

    Update Pneumatics.java

--- src/main/java/frc/robot/Pneumatics.java
+++ src/main/java/frc/robot/Pneumatics.java
@@ -1,5 +1,23 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 public class Pneumatics{
-    
+
+    DoubleSolenoid frntEnd = new DoubleSolenoid(0, 1);
+    DoubleSolenoid backEnd = new DoubleSolenoid(2, 3);
+
+    DoubleSolenoid grabberLiftFrnt = new DoubleSolenoid(4, 5);
+    DoubleSolenoid grabberliftBack = new DoubleSolenoid(6, 7);
+
+    DoubleSolenoid hatchGrabber = new DoubleSolenoid(8, 9);
+
+    public Pneumatics(){
+
+    }
+
+    public void run(){
+        
+    }
+
 }
